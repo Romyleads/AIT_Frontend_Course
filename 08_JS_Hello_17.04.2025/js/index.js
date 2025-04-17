7405e813d68881fc3e819e09278591d8a7542cb3
Romyleads <romyleads@gmail.com>
2025-04-17 11:47:37 +0200

    add lesson 8

---
+++ 08_JS_Hello_17.04.2025/js/index.js
@@ -0,0 +1,39 @@
+console.log('Hello, Alisher!');
+
+// объявили переменную и присвоили ей значение 6
+
+// 7 примитивных типов
+// Тип number
+let x;  // объвить
+x = 10; // присвоить значение = инициализация
+
+console.log(x);
+
+let y = 12.5;
+console.log(y);
+
+x=13;
+x=y;
+console.log(x);
+
+x = 'Maria';
+
+console.log(x);
+let name = '"Alisher"';
+let nameq = "'Alisher'";
+
+console.log(nameq);
+
+// Тип bigint для очень больших чисел
+let z = 123123n;
+
+let message
+console.log(message/0/0);
+
+// Тип null
+
+let secondName = null;
+
+
+
+
