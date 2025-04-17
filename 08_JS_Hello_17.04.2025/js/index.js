b41f6c4217fa1991b35f60386645850ac1f161be
Romyleads <romyleads@gmail.com>
2025-04-17 13:45:16 +0200

    add lesson 8

--- 08_JS_Hello_17.04.2025/js/index.js
+++ 08_JS_Hello_17.04.2025/js/index.js
@@ -35,5 +35,57 @@ console.log(message/0/0);
 let secondName = null;
 
 
+// Не примитив Object
+
+const user = {
+    "email": "khamidov@gmail.com"
+}
+
+// Явное и неявное приведение типов
+// falsy value - те, которые преобразуются в false
+
+
+// Явное
+// 
+
+let elewen = Number("M");
+console.log('elewen');
+
+
+
+console.log("📦1 Полный обзор типов данных в JavaScript:\n");
+
+const types = {
+  string: "Привет, мир!",                             // string
+  number: 123.45,                                     // number
+  bigint: 123456789012345678901234567890n,           // bigint
+  boolean: true,                                      // boolean
+  undefinedType: undefined,                          // undefined
+  nullType: null,                                     // null (typeof → "object")
+  symbol: Symbol("уникальность"),                    // symbol
+
+  object: { name: "Alice", age: 30 },                // object
+  array: [1, 2, 3],                                   // array (object)
+  func: function () { return "Hello from func"; },    // function (object)
+  arrowFunc: () => "Hello from arrow",                // function (object)
+  date: new Date(),                                   // object (Date)
+  regexp: /ab+c/i,                                    // object (RegExp)
+  map: new Map(),                                     // object (Map)
+  set: new Set(),                                     // object (Set)
+  weakMap: new WeakMap(),                             // object (WeakMap)
+  weakSet: new WeakSet(),                             // object (WeakSet)
+  error: new Error("Что-то пошло не так"),            // object (Error)
+  promise: Promise.resolve(123),                      // object (Promise)
+  buffer: new ArrayBuffer(8),                         // object (ArrayBuffer)
+  typedArray: new Uint8Array([1, 2, 3]),              // object (TypedArray)
+};
+
+for (const [key, value] of Object.entries(types)) {
+  const type = typeof value;
+  const constructor = value !== null && value !== undefined ? value.constructor.name : "n/a";
+  console.log(
+    `${key.padEnd(12)} → typeof: ${type.padEnd(10)} | constructor: ${constructor}`
+  );
+}
 
 
---
+++ 08_JS_Hello_17.04.2025/js/index2.js
@@ -0,0 +1,15 @@
+// Создайте две переменные, мой уровень понимания, уровень понимани ядруга маминой подруги
+// мой = 0; Уровень маминой подруги 100
+
+// выполнить проверку и вывести фразу в консоль, ах ты умный такой "You\'re smarty pants"
+
+let myUnderstandingLevel = 0;
+let sunOfMotherFriend = 100;
+
+if(myUnderstandingLevel){
+    console.log("You\'re smarty pants");
+}
+if(sunOfMotherFriend){
+    console.log("He is smarty pants");
+}
+console.log(clear);
