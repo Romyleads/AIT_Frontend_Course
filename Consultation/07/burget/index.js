88c4375446c01d99b9988b4c5fd18593df8b4ece
Romyleads <romyleads@gmail.com>
2025-05-07 17:23:28 +0200

    add consultation 7

---
+++ Consultation/07/burget/index.js
@@ -0,0 +1,8 @@
+const burger_icon = document.querySelector('.burger_icon');
+const nav_menu = document.querySelector('.nav_menu');
+
+burger_icon.addEventListener('click', ()=>nav_menu.classList.toggle ('nav_active')); // добавляет класс nav_active к nav_menu, при єтом удаляет его, если он уже есть
+
+
+
+
---
+++ Consultation/07/index.js
@@ -0,0 +1,21 @@
+// Мы можем создавать элементы с нуля и прикреплять их к документу html
+// создаем новый элемент p
+
+const newElement = document.createElement("p");
+newElement.textContent = "Я БЫЛ СОЗДАН ПРИ ПОМОЩИ JS";
+newElement.style.border ="2px solid red";
+document.body.append(newElement);
+
+
+const targetElement = document.getElementById("target-element");
+const newBtn = document.createElement("button");
+newBtn.type = "button";
+newBtn.textContent = "Нажми на меня";
+
+targetElement.append(newBtn);
+
+
+
+
+
+
